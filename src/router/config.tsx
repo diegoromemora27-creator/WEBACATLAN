dfd3595c23c2bd45a1c9f37aadb676e4d6d5f88e
Diego271-alt <dr87152@gmail.com>
2026-02-04 21:05:02 -0600

    rollback

--- src/router/config.tsx
+++ src/router/config.tsx
@@ -17,15 +17,10 @@ import DevOps from "../pages/devops/page";
 import DockerIntro from "../pages/devops/docker-intro/page";
 import DockerEjemplo from "../pages/devops/docker-ejemplo/page";
 import CloudComputing from "../pages/devops/cloud-computing/page";
-import ArquitecturaPT2 from "../pages/fundamentos-arquitectura/arquitectura-pt2/page";
-import IAParte1 from "../pages/fundamentos-ia/ia-pt1/page";
-import IAParte2 from "../pages/fundamentos-ia/ia-pt2/page";
 
 const HTMLPage = lazy(() => import('../pages/frontend/html/page'));
 const CSSPage = lazy(() => import('../pages/frontend/css/page'));
 const SeguridadTestingPage = lazy(() => import('../pages/seguridad-testing/page'));
-const FundamentosArquitecturaPage = lazy(() => import('../pages/fundamentos-arquitectura/page'));
-const ArquitecturaPT1Page = lazy(() => import('../pages/fundamentos-arquitectura/arquitectura-pt1/page'));
 
 const routes: RouteObject[] = [
   {
@@ -33,24 +28,8 @@ const routes: RouteObject[] = [
     element: <Home />,
   },
   {
-    path: "/fundamentos-3",
-    element: <FundamentosArquitecturaPage />,
-  },
-  {
-    path: "/fundamentos-arquitectura/arquitectura1",
-    element: <ArquitecturaPT1Page />,
-  },
-  {
-    path: "/fundamentos-arquitectura/arquitectura2",
-    element: <ArquitecturaPT2 />,
-  },
-  {
-    path: "/fundamentos-ia/ia-pt1",
-    element: <IAParte1 />,
-  },
-  {
-    path: "/fundamentos-ia/ia-pt2",
-    element: <IAParte2 />,
+    path: "/fundamentos-arquitectura",
+    element: <FundamentosArquitectura />,
   },
   {
     path: "/metodologias-agiles",
